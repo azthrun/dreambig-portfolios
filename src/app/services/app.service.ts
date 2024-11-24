ee37ec24109b8e385e58fcbc65df9e20f889b9d1
Terry Chen <sterryc1986@gmail.com>
2024-11-24 10:39:37 -0600

    added api request auth related attributes

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, Signal } from '@angular/core';
-import { catchError, delay, of } from 'rxjs';
+import { catchError, map, of } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import environment from '../../environments/environment';
 import { Profile } from '../types/profile.type';
@@ -12,11 +12,31 @@ export class AppService {
   private readonly configs = environment;
 
   private profileId: string = environment.profileId;
+  private sessionId?: string;
+  private headers = environment.dataClient;
+
+  private fetchSessionId(): Signal<string | undefined> {
+    var data$ = this.httpClient.post<{ id: string }>(`${this.configs.apiUrl}/sessions`, { 'userAgent': window.navigator.userAgent }, { headers: this.headers }).pipe(
+      map((response) => {
+        this.sessionId = response.id;
+        return response.id;
+      }),
+      catchError((error) => {
+        console.error('Error fetching session', error);
+        this.sessionId = 'anonymous-session';
+        return of("anonymous-session");
+      }),
+    );
+    return toSignal(data$);
+  }
 
   public profileSig = this.fetchProfile();
   private fetchProfile(): Signal<Profile | null | undefined> {
-    var data$ = this.httpClient.get<Profile>(`${this.configs.apiUrl}/profiles/${this.profileId}`).pipe(
-      delay(700),
+    if (this.sessionId === undefined) {
+      this.fetchSessionId()();
+    }
+    const requestHeaders = { ...this.headers, 'Session-Id': this.sessionId ?? 'anonymous-session' };
+    var data$ = this.httpClient.get<Profile>(`${this.configs.apiUrl}/profiles/${this.profileId}`, { headers: requestHeaders }).pipe(
       catchError((error) => {
         console.error('Error fetching profile', error);
         return of(null);
@@ -26,8 +46,11 @@ export class AppService {
   }
 
   public fetchPostLists(): Signal<PostList | undefined> {
-    var data$ = this.httpClient.get<PostList>(`${this.configs.apiUrl}/postLists/${this.profileId}`).pipe(
-      delay(700),
+    if (this.sessionId === undefined) {
+      this.fetchSessionId()();
+    }
+    const requestHeaders = { ...this.headers, 'Session-Id': this.sessionId ?? 'anonymous-session' };
+    var data$ = this.httpClient.get<PostList>(`${this.configs.apiUrl}/postLists/${this.profileId}`, { headers: requestHeaders }).pipe(
       catchError((error) => {
         console.error('Error fetching posts', error);
         return of({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
     profileId: 'e4eaaaf2-d142-11e1-b3e4-080027620cdd',
+    dataClient: { "data-client": "dbp" },
 };
 
 export default environment;
