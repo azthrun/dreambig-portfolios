b0b0feeebcde1339ed433cb8c4fdcc7072cc28ae
Terry Chen <sterryc1986@gmail.com>
2024-11-24 09:42:59 -0600

    implemented chat bot component and added an AI chat bot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,9 @@ export const routes: Routes = [{
     path: 'aboutme',
     loadComponent: () => import('./components/aboutme/aboutme.component').then(m => m.AboutMeComponent),
 }, {
+    path: 'chatbot',
+    loadComponent: () => import('./components/chatbot/chatbot.component').then(m => m.ChatbotComponent),
+},{
     path: '**',
     redirectTo: 'contacts',
 }];
---
+++ src/app/components/chatbot/chatbot.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbp-chatbot',
+  imports: [],
+  templateUrl: './chatbot.component.html',
+  styleUrl: './chatbot.component.scss'
+})
+export class ChatbotComponent {
+
+}
